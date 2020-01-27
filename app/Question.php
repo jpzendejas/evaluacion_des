6baef01697d5f04956c62378d1fa810fda1c1ec2
Juan Pablo <jp@salamanca.gob.mx>
2020-01-27 10:20:59 -0600

    Model Fillable row "question"

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = ['question'];
+
     public function answers(){
       return $this->hasMany('App\QuestionAnswer');
     }
+
+
 }
